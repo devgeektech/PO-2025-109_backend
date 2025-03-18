63df67c57090226a768b021d94be7d10584a3511
deepak-geek <deepak@geekinformatic.com>
2025-03-18 12:01:49 +0530

    changes in firebase

--- dist/src/utils/firebase.js
+++ dist/src/utils/firebase.js
@@ -32,18 +32,14 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const admin = __importStar(require("firebase-admin"));
-const app_1 = require("firebase-admin/app");
-const firebase_json_1 = __importDefault(require("../../config/firebase.json"));
+// import firebaseServiceAccount from '../../config/firebase.json'; 
 if (admin.apps.length === 0) {
-    let file = firebase_json_1.default;
-    (0, app_1.initializeApp)({
-        credential: admin.credential.cert(file),
-    });
+    // let file: any = firebaseServiceAccount;
+    // initializeApp({
+    //   credential: admin.credential.cert(file),
+    // });
 }
 exports.default = admin;
 //# sourceMappingURL=firebase.js.map
